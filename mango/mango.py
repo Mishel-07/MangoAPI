e19484782a79e39c7b899cbe0123e6fdba2fd975
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-18 21:08:28 +0530

    Update mango.py

--- mango/mango.py
+++ mango/mango.py
@@ -1,16 +1,17 @@
 import httpx
+from utils import BASE_URL, Chat
 
 class Mango:
     """
     A class to generate content using AI models.
     """
 
-    def __init__(self, base_url="https://horrid-api.vercel.app/mango", **kwargs):
+    def __init__(self, base_url=BASE_URL, **kwargs):
         """
         Initialize the class with the base URL of the API.
 
         Args:
-            base_url (str, optional): The base URL of the API. Defaults to "https://horridapi.onrender.com/mango".
+            base_url (str, optional): The base URL of the API. Defaults to "BASE_URL".
         """
         self.base_url = base_url   
         self.session = httpx.Client()
