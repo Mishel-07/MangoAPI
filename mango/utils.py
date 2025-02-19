37af6847b4d9c6805fd8dab78af166a9f78e7ba4
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-19 11:10:31 +0530

    Update utils.py

--- mango/utils.py
+++ mango/utils.py
@@ -16,7 +16,7 @@ class Completions:
             raise ValueError("An error Report @XBOTSUPPORTS or https://github.com/Mishel-07/MangoAPI/issues")
         ms = {'messages': messages, 'model': model}                
         try:
-            response = self.chat.mango.session.post("mango", json=ms)
+            response = self.chat.mango.session.post(f"{self.chat.mango.base_url}/mango", json=ms)
             k = response.json()
             if "messages" in k and "invalid model" in k["messages"]:
                 raise ValueError("Invalid model")
