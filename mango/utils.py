f0b3777e421977c232541a59cefbf842675e2d31
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-20 20:03:42 +0530

    Update utils.py

--- mango/utils.py
+++ mango/utils.py
@@ -3,6 +3,10 @@ BASE_URL = "https://mangooapi.onrender.com"
 def gpt_4o():
     return 'gpt-4o'
 
+def blackbox():
+    return 'blackbox'
+    
 class Models:
   def __init__(self):
-      self.gpt_4o = gpt_4o()
+      self.gpt4o = gpt_4o()
+      self.blackbox = blackbox()
